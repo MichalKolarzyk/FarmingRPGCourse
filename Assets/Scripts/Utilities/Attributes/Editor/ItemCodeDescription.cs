e2daca4828490300a569a64712ce7be8307542a4
LAPTOP-L1T0L126\micha <michalkolarzyk@gmail.com>
2024-09-11 21:45:34 +0200

    Minor changes

--- Assets/Scripts/Utilities/Attributes/Editor/ItemCodeDescription.cs
+++ Assets/Scripts/Utilities/Attributes/Editor/ItemCodeDescription.cs
@@ -8,4 +8,21 @@ public class ItemCodeDescription : PropertyDrawer
     {
         return EditorGUI.GetPropertyHeight(property) * 2;
     }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+    //     EditorGUI.BeginProperty(position, label, property);
+    //     if(property.propertyType == SerializedPropertyType.Integer){
+
+    //         EditorGUI.BeginChangeCheck();
+
+    //         var newValue = EditorGUI.IntField()
+
+    //         if(EditorGUI.EndChangeCheck()){
+    //             property.intValue = newValue;
+    //         }
+
+    //     }
+    //     EditorGUI.EndProperty();
+    }
 }
