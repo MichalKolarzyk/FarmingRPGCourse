50cf68bf204eb434f9ebca0b943a6ba684744565
LAPTOP-L1T0L126\micha <michalkolarzyk@gmail.com>
2024-10-17 20:58:22 +0200

    Minor changes

--- Assets/Scripts/Application/Inventories/PickUpItemBehaviour.cs
+++ Assets/Scripts/Application/Inventories/PickUpItemBehaviour.cs
@@ -1,21 +1,24 @@
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(Context<Inventory>))]
 public class PickUpItemBehaviour : MonoBehaviour
 {
     private Context<Inventory> inventoryContext;
-    private CollectionContext<Item> collectionItemContext;
+    private Lazy<CollectionContext<Item>> collectionItemContextLazy;
 
     void Awake()
     {
         inventoryContext = GetComponent<Context<Inventory>>();
+        collectionItemContextLazy = new Lazy<CollectionContext<Item>>(() => FindObjectOfType<CollectionContext<Item>>());
     }
 
 
     void OnTriggerEnter2D(Collider2D collider2D)
     {
         var inventory = inventoryContext.Get();
-        collectionItemContext = FindObjectOfType<CollectionContext<Item>>();
+        var collectionItemContext = collectionItemContextLazy.Value;
+
         if (collider2D.TryGetComponent(out ItemContext itemContext))
         {
             var canAddToInventory = inventory.TryAdd(itemContext.ToInventoryItemModel());
